387d121972d40f8c2cb3378a1ae7af1d52ac10e6
Sangeetharaj <sangeetharajs.gound@hcl.com>
2020-05-28 09:39:37 +0530

    Project Alpha created

---
+++ projectAlpha/src/test/java/com/example/projectAlpha/ProjectAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectAlpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
